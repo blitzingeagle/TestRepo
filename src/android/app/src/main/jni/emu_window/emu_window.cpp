d5d3923d025e73b73aaee6a5f1948acd5709d992
PabloMK7 <hackyglitch2@gmail.com>
2024-07-16 23:54:02 +0200

    renderer_vulkan: Address vulkan surface recreation issues (#198)

--- src/android/app/src/main/jni/emu_window/emu_window.cpp
+++ src/android/app/src/main/jni/emu_window/emu_window.cpp
@@ -23,13 +23,11 @@ bool EmuWindow_Android::OnSurfaceChanged(ANativeWindow* surface) {
     }
 
     render_window = surface;
-
     window_info.type = Frontend::WindowSystemType::Android;
     window_info.render_surface = surface;
 
     StopPresenting();
     OnFramebufferSizeChanged();
-
     return true;
 }
 
