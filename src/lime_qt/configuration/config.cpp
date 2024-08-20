f2f41397684a7a93c8e94d4a5527f7e842d4eefb
szdarkhack <1457198+szdarkhack@users.noreply.github.com>
2024-08-20 22:04:29 +0300

    Add "Steps per hour" system setting (#211)
    * Implements a steps per hour (global) setting that gets returned by PTM GetStepHistory
    
    * Make setting label text clearer
    
    * Add setting to SDL frontend
    
    * Add setting to Android (no UI)
    
    * Remove IntSetting enum value
    
    * Follow convension in android default ini

--- src/lime_qt/configuration/config.cpp
+++ src/lime_qt/configuration/config.cpp
@@ -731,6 +731,7 @@ void QtConfig::ReadSystemValues() {
         ReadBasicSetting(Settings::values.init_time_offset);
         ReadBasicSetting(Settings::values.init_ticks_type);
         ReadBasicSetting(Settings::values.init_ticks_override);
+        ReadBasicSetting(Settings::values.steps_per_hour);
         ReadBasicSetting(Settings::values.plugin_loader_enabled);
         ReadBasicSetting(Settings::values.allow_plugin_loader);
     }
@@ -1261,6 +1262,7 @@ void QtConfig::SaveSystemValues() {
         WriteBasicSetting(Settings::values.init_time_offset);
         WriteBasicSetting(Settings::values.init_ticks_type);
         WriteBasicSetting(Settings::values.init_ticks_override);
+        WriteBasicSetting(Settings::values.steps_per_hour);
         WriteBasicSetting(Settings::values.plugin_loader_enabled);
         WriteBasicSetting(Settings::values.allow_plugin_loader);
     }
