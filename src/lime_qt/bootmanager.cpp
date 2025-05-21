SiniKraft/azahar

qt: For Qt 6.9.0 and above, use QImage::flipped over QImage::mirrored
The latter has been deprecated, and is causing build failures where deprecations warnings are treated as errors.

src/citra_qt/bootmanager.cpp
@@ -10,6 +10,7 @@
 #include <QWindow>
 #include "citra_qt/bootmanager.h"
 #include "citra_qt/citra_qt.h"
+#include "citra_qt/util/util.h"
 #include "common/color.h"
 #include "common/microprofile.h"
 #include "common/scm_rev.h"
@@ -714,7 +715,7 @@ void GRenderWindow::CaptureScreenshot(u32 res_scale, const QString& screenshot_p
         screenshot_image.bits(),
         [this, screenshot_path](bool invert_y) {
             const std::string std_screenshot_path = screenshot_path.toStdString();
-            if (screenshot_image.mirrored(false, invert_y).save(screenshot_path)) {
+            if (GetMirroredImage(screenshot_image, false, invert_y).save(screenshot_path)) {
                 LOG_INFO(Frontend, "Screenshot saved to \"{}\"", std_screenshot_path);
             } else {
                 LOG_ERROR(Frontend, "Failed to save screenshot to \"{}\"", std_screenshot_path);

src/citra_qt/camera/camera_util.cpp
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <QImage>
 #include "citra_qt/camera/camera_util.h"
+#include "citra_qt/util/util.h"
 
 namespace CameraUtil {
 
@@ -213,9 +214,9 @@ std::vector<u16> ProcessImage(const QImage& image, int width, int height, bool o
     }
     QImage scaled =
         image.scaled(width, height, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
-    QImage transformed =
-        scaled.copy((scaled.width() - width) / 2, (scaled.height() - height) / 2, width, height)
-            .mirrored(flip_horizontal, flip_vertical);
+    QImage transformed = GetMirroredImage(
+        scaled.copy((scaled.width() - width) / 2, (scaled.height() - height) / 2, width, height),
+        flip_horizontal, flip_vertical);
     if (output_rgb) {
         QImage converted = transformed.convertToFormat(QImage::Format_RGB16);
         std::memcpy(buffer.data(), converted.bits(), width * height * sizeof(u16));

blitzingeagle/TestRepo

src/lime_qt/bootmanager.cpp
@@ -21,6 +21,7 @@
 #include "input_common/motion_emu.h"
 #include "lime_qt/bootmanager.h"
 #include "lime_qt/lime_qt.h"
+#include "lime_qt/util/util.h"
 #include "video_core/custom_textures/custom_tex_manager.h"
 #include "video_core/gpu.h"
 #include "video_core/renderer_base.h"
@@ -714,7 +715,7 @@ void GRenderWindow::CaptureScreenshot(u32 res_scale, const QString& screenshot_p
         screenshot_image.bits(),
         [this, screenshot_path](bool invert_y) {
             const std::string std_screenshot_path = screenshot_path.toStdString();
-            if (screenshot_image.mirrored(false, invert_y).save(screenshot_path)) {
+            if (GetMirroredImage(screenshot_image, false, invert_y).save(screenshot_path)) {
                 LOG_INFO(Frontend, "Screenshot saved to \"{}\"", std_screenshot_path);
             } else {
                 LOG_ERROR(Frontend, "Failed to save screenshot to \"{}\"", std_screenshot_path);

src/lime_qt/camera/camera_util.cpp
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <QImage>
 #include "lime_qt/camera/camera_util.h"
+#include "lime_qt/util/util.h"
 
 namespace CameraUtil {
 
@@ -213,9 +214,9 @@ std::vector<u16> ProcessImage(const QImage& image, int width, int height, bool o
     }
     QImage scaled =
         image.scaled(width, height, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
-    QImage transformed =
-        scaled.copy((scaled.width() - width) / 2, (scaled.height() - height) / 2, width, height)
-            .mirrored(flip_horizontal, flip_vertical);
+    QImage transformed = GetMirroredImage(
+        scaled.copy((scaled.width() - width) / 2, (scaled.height() - height) / 2, width, height),
+        flip_horizontal, flip_vertical);
     if (output_rgb) {
         QImage converted = transformed.convertToFormat(QImage::Format_RGB16);
         std::memcpy(buffer.data(), converted.bits(), width * height * sizeof(u16));